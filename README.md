e9b438e156d504cad44b84be1808a54812856baa
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-03-13 20:41:50 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# dojo basketball assignment
+Set up a new file and add the Player class with the given constructor -- DONE
+
+Challenge 1: Update the constructor to accept a dictionary (player) as an argument and set the attributes using the dictionary -- DONE
+
+Complete challenge 2: Create 3 instances of the Player class using the given dictionaries -- DONE
+
+Complete challenge 3: Populate a new list with Player instances from the list of players. -- DONE
+
+Ninja Bonus: Add an @class method called get_team(cls, team_list) that, given a list of dictionaries populates and returns a new list of Player objects.
---
+++ basketball.py
@@ -0,0 +1,59 @@
+class Player:
+    def __init__(self, player_data):
+        self.name = player_data['name']
+        self.age = player_data['age']
+        self.position = player_data['position']
+        self.team = player_data['team']
+
+    @classmethod
+    def get_team(cls, team_list):
+        new_team = []
+        for player in team_list:
+            new_player = cls(player)
+            new_team.append(new_player)
+        return new_team
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33, "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32, "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+    	"name": "", 
+    	"age":16, 
+    	"position": "P", 
+    	"team": "en"
+    }
+]
+
+new_team = []
+
+for player in players:
+    new_team.append(Player(player))
+
+print(new_team)
+
+print(Player.get_team(players))
